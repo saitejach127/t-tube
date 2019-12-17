4827befb36295387944fdd107d5ebba22bdbc594
saitejach127 <saitejach127@gmail.com>
2019-12-17 12:42:09 +0530

    port and procfile

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 var app = express();
 
 app.use('/static',express.static("public"))
+app.use('/torrents',express.static("torrents"))
 
 app.get('/',(req,res) => {
     createTorrent('videos/download.mp4',{ urlList:'http://localhost:3000/static/download.mp4' }, (err,tor) => {
-        fs.writeFile('seed.torrent', tor,() => { console.log("created") })
-    })
-    res.send("<h1> Welcome</h1>")
+        fs.writeFile('torrents/seed.torrent', tor,() => { console.log("created") });
+    });
+
+    res.send("<h1> Welcome</h1>");
 })
 
-
-
-app.listen(3000, () => { console.log("Server on 3000") });
+app.listen(process.env.PORT || 3000, () => { console.log("Server on 3000") });
